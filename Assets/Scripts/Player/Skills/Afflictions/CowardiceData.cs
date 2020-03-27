28d7ad249f652e05e20e305d8b6f3319d7463b4e
Ian Chy <chrisian023@gmail.com>
2020-03-27 18:17:14 +0800

    +cowardice, pyrophobia, narcolepsy afflictions

---
+++ Assets/Scripts/Player/Skills/Afflictions/CowardiceData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Traits;
+using UnityEngine;
+
+public class CowardiceData : SpellData {
+    public override SPELL_TYPE type => SPELL_TYPE.COWARDICE;
+    public override string name { get { return "Cowardice"; } }
+    public override string description { get { return "The target will be a scaredy wimp."; } }
+    public override SPELL_CATEGORY category { get { return SPELL_CATEGORY.AFFLICTION; } }
+
+    public CowardiceData() : base() {
+        targetTypes = new SPELL_TARGET[] { SPELL_TARGET.CHARACTER };
+    }
+
+    #region Overrides
+    public override void ActivateAbility(IPointOfInterest targetPOI) {
+        targetPOI.traitContainer.AddTrait(targetPOI, "Coward");
+        Log log = new Log(GameManager.Instance.Today(), "Character", "NonIntel", "player_afflicted");
+        log.AddToFillers(targetPOI, targetPOI.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
+        log.AddToFillers(null, name, LOG_IDENTIFIER.STRING_1);
+        log.AddLogToInvolvedObjects();
+        PlayerManager.Instance.player.ShowNotificationFromPlayer(log);
+        base.ActivateAbility(targetPOI);
+    }
+    public override bool CanPerformAbilityTowards(Character targetCharacter) {
+        if (targetCharacter.isDead || targetCharacter.traitContainer.HasTrait("Coward")) {
+            return false;
+        }
+        return base.CanPerformAbilityTowards(targetCharacter);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Player/Skills/Afflictions/PyrophopiaData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Traits;
+using UnityEngine;
+
+public class PyrophobiaData : SpellData {
+    public override SPELL_TYPE type => SPELL_TYPE.PYROPHOBIA;
+    public override string name { get { return "Pyrophobia"; } }
+    public override string description { get { return "The target will be deathly afraid of Fire."; } }
+    public override SPELL_CATEGORY category { get { return SPELL_CATEGORY.AFFLICTION; } }
+
+    public PyrophobiaData() : base() {
+        targetTypes = new SPELL_TARGET[] { SPELL_TARGET.CHARACTER };
+    }
+
+    #region Overrides
+    public override void ActivateAbility(IPointOfInterest targetPOI) {
+        targetPOI.traitContainer.AddTrait(targetPOI, "Pyrophobic");
+        Log log = new Log(GameManager.Instance.Today(), "Character", "NonIntel", "player_afflicted");
+        log.AddToFillers(targetPOI, targetPOI.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
+        log.AddToFillers(null, name, LOG_IDENTIFIER.STRING_1);
+        log.AddLogToInvolvedObjects();
+        PlayerManager.Instance.player.ShowNotificationFromPlayer(log);
+        base.ActivateAbility(targetPOI);
+    }
+    public override bool CanPerformAbilityTowards(Character targetCharacter) {
+        if (targetCharacter.isDead || targetCharacter.traitContainer.HasTrait("Pyrophobic")) {
+            return false;
+        }
+        return base.CanPerformAbilityTowards(targetCharacter);
+    }
+    #endregion
+}
