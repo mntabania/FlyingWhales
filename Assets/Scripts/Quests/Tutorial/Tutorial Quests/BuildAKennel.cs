89b6891c8ec2f7eca6ccdb8065a6f95ee2700c2c
Mykel Abania <mntabania@gmail.com>
2020-05-13 15:03:40 +0800

    *Kennel Tutorial Fix

--- Assets/Scripts/Quests/Tutorial/Tutorial Quests/BuildAKennel.cs
+++ Assets/Scripts/Quests/Tutorial/Tutorial Quests/BuildAKennel.cs
@@ -15,6 +15,7 @@ namespace Tutorial {
             _activationCriteria = new List<TutorialQuestCriteria>() {
                 new HasCompletedTutorialQuest(TutorialManager.Tutorial.Torture_Chambers)
             };
+            Messenger.AddListener<LocationStructure>(Signals.STRUCTURE_OBJECT_PLACED, OnAlreadyBuiltStructure);
         }
         protected override bool HasMetAllCriteria() {
             bool hasMetAllCriteria = base.HasMetAllCriteria();
@@ -31,9 +32,38 @@ namespace Tutorial {
                     new ClickOnEmptyAreaStep(), 
                     new ObjectPickerShownStep("Click on Build Structure button", "Demonic Structure"), 
                     new StructureBuiltStep(STRUCTURE_TYPE.THE_KENNEL, "Build a Kennel")
-                ),
+                    ),
                 new QuestStepCollection(new DropCharacterAtStructureStep(STRUCTURE_TYPE.THE_KENNEL, typeof(Summon), "Seize a monster and drop it at the Kennel."))
             };
         }
+        public override void Activate() {
+            base.Activate();
+            //stop listening for structure building, since another listener will be used to listen for step completion
+            Messenger.RemoveListener<LocationStructure>(Signals.STRUCTURE_OBJECT_PLACED, OnAlreadyBuiltStructure);
+        }
+        public override void Deactivate() {
+            base.Deactivate();
+            //remove listener, this is for when the tutorial is completed without it being activated 
+            Messenger.RemoveListener<LocationStructure>(Signals.STRUCTURE_OBJECT_PLACED, OnAlreadyBuiltStructure);
+        }
+
+        #region Availability Listeners
+        private void OnAlreadyBuiltStructure(LocationStructure structure) {
+            if (structure is Inner_Maps.Location_Structures.TheKennel) {
+                CompleteQuest(); //player already built a kennel
+            }
+        }
+        #endregion
+
+        #region Step Completion Actions
+        private void OnKennelBuilt() {
+            UIManager.Instance.generalConfirmationWithVisual.ShowGeneralConfirmation("Demonic Structures",
+                "These are unique demonic structures that you can build on unoccupied Areas. " +
+                "Each structure type has a unique use that may aid you in your invasion. For example, " +
+                "the Kennel allows you to take any monster you manage to keep within it, " +
+                "and retain it for future use in a different playthrough.",
+                TutorialManager.Instance.demonicStructureVideoClip);
+        }
+        #endregion
     }
 }
