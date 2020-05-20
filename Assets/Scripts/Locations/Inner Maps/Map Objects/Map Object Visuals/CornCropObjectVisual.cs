f9021fdb5a9f7a38cff98c2dfdb71ca100779a6f
Mykel Abania <mntabania@gmail.com>
2020-05-20 10:18:02 +0800

    +Updated Corn Crops

--- Assets/Scripts/Locations/Inner Maps/Map Objects/Map Object Visuals/CornCropObjectVisual.cs
+++ Assets/Scripts/Locations/Inner Maps/Map Objects/Map Object Visuals/CornCropObjectVisual.cs
@@ -3,12 +3,7 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 public class CornCropObjectVisual : TileObjectGameObject {
-
-    [Tooltip("This is used to determine what growing sprite should be used given a ripe sprite")]
-    [SerializeField] private SpriteSpriteDictionary growingSprites;
-    [Tooltip("This is used to determine what ripe sprite should be used given a growing sprite")]
-    [SerializeField] private SpriteSpriteDictionary ripeSprites;
-
+    
     [SerializeField] private Sprite empty;
     [SerializeField] private Sprite growing;
     [SerializeField] private Sprite harvestable;
@@ -18,23 +13,12 @@ public class CornCropObjectVisual : TileObjectGameObject {
         Assert.IsNotNull(crops, $"Given tile object for {gameObject.name} is not a crop! {tileObject}");
         int totalRipeningTicks = crops.GetRipeningTicks();
         int ticksGrowing = totalRipeningTicks - crops.remainingRipeningTicks; 
-        if (ticksGrowing < Mathf.FloorToInt(totalRipeningTicks * 0.5f)) {
+        if (ticksGrowing < Mathf.FloorToInt(totalRipeningTicks * 0.4f)) {
             SetVisual(empty);
         } else if (ticksGrowing < totalRipeningTicks) {
             SetVisual(growing);    
         } else {
             SetVisual(harvestable);
         }
-        
-        
-        // if (crops.currentGrowthState == Crops.Growth_State.Growing) {
-        //     if (growingSprites.Values.Contains(objectVisual.sprite)) { return; } //current sprite is already set to growing sprite
-        //     Assert.IsTrue(growingSprites.ContainsKey(objectVisual.sprite), $"Growing sprite for {name} could not be found using sprite {objectVisual.sprite}");
-        //     SetVisual(growingSprites[objectVisual.sprite]);
-        // } else if (crops.currentGrowthState == Crops.Growth_State.Ripe) {
-        //     if (ripeSprites.Values.Contains(objectVisual.sprite)) { return; } //current sprite is already set to ripe sprite
-        //     Assert.IsTrue(ripeSprites.ContainsKey(objectVisual.sprite), $"Ripe sprite for {name} could not be found using sprite {objectVisual.sprite}");
-        //     SetVisual(ripeSprites[objectVisual.sprite]);
-        // }
     }
 }
