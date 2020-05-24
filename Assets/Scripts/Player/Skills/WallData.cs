d1fcdea36f54a637efe7259718965b9fda7e8eef
Mykel Abania <mntabania@gmail.com>
2020-05-24 18:31:06 +0800

    +More Tooltip Images/Videos

--- Assets/Scripts/Player/Skills/WallData.cs
+++ Assets/Scripts/Player/Skills/WallData.cs
@@ -11,4 +11,22 @@ public class WallData : SpellData {
     public WallData() : base() {
         targetTypes = new SPELL_TARGET[] { SPELL_TARGET.TILE };
     }
+
+    public override void ActivateAbility(LocationGridTile targetTile) {
+        BlockWall wall = InnerMapManager.Instance.CreateNewTileObject<BlockWall>(TILE_OBJECT_TYPE.BLOCK_WALL);
+        wall.SetWallType(WALL_TYPE.Demon_Stone);
+        targetTile.structure.AddPOI(wall, targetTile);
+        //IncreaseThreatThatSeesTile(targetTile, 10);
+        base.ActivateAbility(targetTile);
+    }
+    public override bool CanPerformAbilityTowards(LocationGridTile targetTile) {
+        bool canPerform = base.CanPerformAbilityTowards(targetTile);
+        if (canPerform) {
+            return targetTile.structure != null && targetTile.objHere == null;
+        }
+        return canPerform;
+    }
+    public override void HighlightAffectedTiles(LocationGridTile tile) {
+        TileHighlighter.Instance.PositionHighlight(0, tile);
+    }    
 }
