85ffd2e880853879a44761052a61d1ff3a6db7c1
Mykel Abania <mntabania@gmail.com>
2020-05-27 19:00:37 +0800

    *HexTile rescan pathfinding function

--- Assets/Scripts/Locations/Inner Maps/Building Spots/LocationGridTileCollectionItem.cs
+++ Assets/Scripts/Locations/Inner Maps/Building Spots/LocationGridTileCollectionItem.cs
@@ -6,6 +6,8 @@ using TMPro;
 using UnityEngine;
 
 public class LocationGridTileCollectionItem : MonoBehaviour {
+
+    [SerializeField] private Collider2D boundsCollider;
     
     void OnDrawGizmos() {
         Gizmos.color = Color.blue;
@@ -13,4 +15,11 @@ public class LocationGridTileCollectionItem : MonoBehaviour {
         Gizmos.DrawWireCube(position, new Vector3(InnerMapManager.BuildingSpotSize.x - 0.5f, InnerMapManager.BuildingSpotSize.y - 0.5f, 0));    
     }
 
+
+    [ContextMenu("Update Pathfinding Graphs")]
+    public void UpdatePathfindingGraph() {
+        AstarPath.active.UpdateGraphs(boundsCollider.bounds);
+    }
+    
+    
 }
