6bccccc38969419a94b562ed43dbfe74f90b07c0
Ian Chy <chrisian023@gmail.com>
2020-06-12 13:51:10 +0800

    *bug fix - Dead characters shouldn't trigger Inspire

--- Assets/Scripts/Traits/Normal Traits/Inspiring.cs
+++ Assets/Scripts/Traits/Normal Traits/Inspiring.cs
@@ -19,18 +19,20 @@ namespace Traits {
                 Character targetCharacter = targetPOI as Character;
                 string debugLog = $"{characterThatWillDoJob.name} saw {targetPOI.name} and has {name}";
                 debugLog += "\n-20% chance to trigger Inspired interrupt if Target is part of Faction or NPCSettlement";
-                if (characterThatWillDoJob.faction == targetCharacter.faction || characterThatWillDoJob.homeSettlement == targetCharacter.homeSettlement) {
-                    debugLog += "\n-Target is part of Faction or NPCSettlement";
-                    int chance = UnityEngine.Random.Range(0, 100);
-                    debugLog += $"\n-Roll: {chance}";
-                    if (chance < 8) {
-                        debugLog += "\n-Triggered Inspired interrupt";
-                        characterThatWillDoJob.logComponent.PrintLogIfActive(debugLog);
-                        targetCharacter.interruptComponent.TriggerInterrupt(INTERRUPT.Inspired, characterThatWillDoJob);
-                        return true;
+                if (characterThatWillDoJob.isNormalCharacter && targetCharacter.isNormalCharacter && !characterThatWillDoJob.isDead && !targetCharacter.isDead) {
+                    if (characterThatWillDoJob.faction == targetCharacter.faction || characterThatWillDoJob.homeSettlement == targetCharacter.homeSettlement) {
+                        debugLog += "\n-Target is part of Faction or NPCSettlement";
+                        int chance = UnityEngine.Random.Range(0, 100);
+                        debugLog += $"\n-Roll: {chance}";
+                        if (chance < 8) {
+                            debugLog += "\n-Triggered Inspired interrupt";
+                            characterThatWillDoJob.logComponent.PrintLogIfActive(debugLog);
+                            targetCharacter.interruptComponent.TriggerInterrupt(INTERRUPT.Inspired, characterThatWillDoJob);
+                            return true;
+                        }
+                    } else {
+                        debugLog += "\n-Target is part of Faction or NPCSettlement";
                     }
-                } else {
-                    debugLog += "\n-Target is part of Faction or NPCSettlement";
                 }
                 characterThatWillDoJob.logComponent.PrintLogIfActive(debugLog);
             }
