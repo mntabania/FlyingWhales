30e9131add0ad64dc5e6383a6a5849a9f3bbb95a
Mykel Abania <mntabania@gmail.com>
2020-07-29 17:13:09 +0800

    **Fixes**
     - Spider Hatch Fix
     - Being Brainwashed fix
     - Carried character affecting nameplate state fix

--- Assets/Scripts/Interrupts/BeingBrainwashed.cs
+++ Assets/Scripts/Interrupts/BeingBrainwashed.cs
@@ -16,23 +16,20 @@ namespace Interrupts {
 
         #region Overrides
         public override bool ExecuteInterruptEndEffect(InterruptHolder interruptHolder) {
-            Assert.IsTrue(interruptHolder.actor.gridTileLocation.structure.IsTilePartOfARoom(interruptHolder.actor.gridTileLocation,
-                out var room) && room is DefilerRoom);
-
-            DefilerRoom defilerRoom = room as DefilerRoom;
-            
-            Log log;
-            if (defilerRoom.WasBrainwashSuccessful(interruptHolder.actor)) {
-                //successfully converted
-                interruptHolder.actor.traitContainer.AddTrait(interruptHolder.actor, "Cultist");
-                log = new Log(GameManager.Instance.Today(), "Interrupt", "Being Brainwashed", "converted");
-            } else {
-                interruptHolder.actor.traitContainer.AddTrait(interruptHolder.actor, "Unconscious");
-                log = new Log(GameManager.Instance.Today(), "Interrupt", "Being Brainwashed", "not_converted");
+            if (interruptHolder.actor.gridTileLocation.structure.IsTilePartOfARoom(interruptHolder.actor.gridTileLocation,
+                out var room) && room is DefilerRoom defilerRoom) {
+                Log log;
+                if (defilerRoom.WasBrainwashSuccessful(interruptHolder.actor)) {
+                    //successfully converted
+                    interruptHolder.actor.traitContainer.AddTrait(interruptHolder.actor, "Cultist");
+                    log = new Log(GameManager.Instance.Today(), "Interrupt", "Being Brainwashed", "converted");
+                } else {
+                    interruptHolder.actor.traitContainer.AddTrait(interruptHolder.actor, "Unconscious");
+                    log = new Log(GameManager.Instance.Today(), "Interrupt", "Being Brainwashed", "not_converted");
+                }
+                log.AddToFillers(interruptHolder.actor, interruptHolder.actor.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
+                interruptHolder.actor.logComponent.RegisterLog(log, onlyClickedCharacter: false);   
             }
-            log.AddToFillers(interruptHolder.actor, interruptHolder.actor.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
-            interruptHolder.actor.logComponent.RegisterLog(log, onlyClickedCharacter: false);
-
             return true;
         }
         #endregion
