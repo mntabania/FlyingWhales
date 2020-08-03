994de3809b6ae921a71f243654ce6aea93be4bb1
Mykel Abania <mntabania@gmail.com>
2020-08-04 00:42:46 +0800

    **New**
     - Restrain and Apprehend job changes
     - Patrol behaviour fix

--- Assets/Scripts/Characters/Behaviour/PatrolBehaviour.cs
+++ Assets/Scripts/Characters/Behaviour/PatrolBehaviour.cs
@@ -10,13 +10,18 @@ public class PatrolBehaviour : CharacterBehaviourComponent {
         priority = 450;
     }
     public override bool TryDoBehaviour(Character character, ref string log, out JobQueueItem producedJob) {
-        LocationStructure chosenStructure = character.currentRegion.GetRandomStructure();
+        LocationStructure chosenStructure;
+        if (character.homeSettlement != null) {
+            chosenStructure = character.homeSettlement.GetRandomStructure();
+        } else {
+            chosenStructure = character.currentRegion.GetRandomStructure();
+        }
         LocationGridTile chosenTile = chosenStructure.GetRandomTile();
         GoapPlanJob job =
             JobManager.Instance.CreateNewGoapPlanJob(JOB_TYPE.PATROL, INTERACTION_TYPE.PATROL, character, character);
         job.AddOtherData(INTERACTION_TYPE.PATROL, new object[]{ chosenTile });
         job.SetCannotBePushedBack(true);
-        producedJob = job;
+        producedJob = job;    
         return true;
     }
 
