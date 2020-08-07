028e094f2e3baaaf4a559c6cc884e57e04cd4f8b
Mykel Abania <mntabania@gmail.com>
2020-08-08 00:30:40 +0800

    +Defiler should close door when dropping character *Griefstricken mood effect changes

--- Assets/Scripts/Locations/Inner Maps/Location Structures/Defiler.cs
+++ Assets/Scripts/Locations/Inner Maps/Location Structures/Defiler.cs
@@ -7,6 +7,14 @@ namespace Inner_Maps.Location_Structures {
         public Defiler(Region location) : base(STRUCTURE_TYPE.DEFILER, location) {
             selectableSize = new Vector2(10f, 10f);
         }
+        
+        public override void OnCharacterUnSeizedHere(Character character) {
+            base.OnCharacterUnSeizedHere(character);
+            if (character.gridTileLocation != null && IsTilePartOfARoom(character.gridTileLocation, out var room)) {
+                DoorTileObject door = room.GetTileObjectInRoom<DoorTileObject>(); //close door in room
+                door?.Close();
+            }
+        }
 
         #region Rooms
         protected override StructureRoom CreteNewRoomForStructure(List<LocationGridTile> tilesInRoom) {
