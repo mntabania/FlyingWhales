4ef14871678e8b53bbb3fe5d4a7e8fde8035e3ba
Ian Chy <chrisian023@gmail.com>
2020-08-08 01:08:31 +0800

    *character vision changes

--- Assets/Scripts/Characters/Character Marker/CharacterMarkerVisionCollider.cs
+++ Assets/Scripts/Characters/Character Marker/CharacterMarkerVisionCollider.cs
@@ -139,7 +139,7 @@ public class CharacterMarkerVisionCollider : BaseVisionCollider {
 
     #region Different Structure Handling
     private void OnCharacterArrivedAtStructure(Character character, LocationStructure structure) {
-        if (parentMarker.character.combatComponent.isInCombat) { return; } //if character is in combat, ignore this
+        //if (parentMarker.character.combatComponent.isInCombat) { return; } //if character is in combat, ignore this
          //if the character that arrived at the new structure is in this character different structure list
          //check if that character now has the same structure as this character,
         if (parentMarker.inVisionPOIsButDiffStructure.Contains(character) && (structure == parentMarker.character.currentStructure || (structure.structureType.IsOpenSpace() && parentMarker.character.currentStructure.structureType.IsOpenSpace()))) {
