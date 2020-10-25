3ef6b935bcdcaa544bd28cb19417d7113cf760e0
Mykel Abania <mntabania@gmail.com>
2020-10-25 18:56:07 +0800

    *Increased project version to 0.33.30 *Set crime severity of Nature Worship and Divine Worship if character is Demon Worshipper to Serious

--- Assets/Scripts/Crime/Crime Types/NatureWorship.cs
+++ Assets/Scripts/Crime/Crime Types/NatureWorship.cs
@@ -12,6 +12,12 @@ namespace Crime_System {
         public NatureWorship() : base(CRIME_TYPE.Nature_Worship) { }
 
         #region Overrides
+        public override CRIME_SEVERITY GetCrimeSeverity(Character witness, Character actor, IPointOfInterest target) {
+            if (witness.religionComponent.religion == RELIGION.Demon_Worship) {
+                return CRIME_SEVERITY.Serious;
+            }
+            return base.GetCrimeSeverity(witness, actor, target);
+        }
         public override string GetLastStrawReason(Character witness, Character actor, IPointOfInterest target, ICrimeable crime) {
             return actor.name + " worships Nature";
         }
