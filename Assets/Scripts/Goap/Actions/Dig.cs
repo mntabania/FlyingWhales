8aae71f3ce2305633ee4a54faeea6e9e09728bac
Ian Chy <chrisian023@gmail.com>
2020-11-03 17:48:48 +0800

    *dig bug fix

--- Assets/Scripts/Goap/Actions/Dig.cs
+++ Assets/Scripts/Goap/Actions/Dig.cs
@@ -32,8 +32,17 @@ public class Dig : GoapAction {
         }
         return false;
     }
+    public override GoapActionInvalidity IsInvalid(ActualGoapNode node) {
+        Character actor = node.actor;
+        IPointOfInterest poiTarget = node.poiTarget;
+
+        string stateName = "Target Missing";
+        bool defaultTargetMissing = IsTargetMissing(node);
+        GoapActionInvalidity goapActionInvalidity = new GoapActionInvalidity(defaultTargetMissing, stateName);
+        return goapActionInvalidity;
+    }
     #endregion
-    
+
     #region State Effects
     [UsedImplicitly]
     public void AfterDigSuccess(ActualGoapNode goapNode) {
@@ -41,4 +50,31 @@ public class Dig : GoapAction {
         // goapNode.poiTarget.gridTileLocation.structure.RemovePOI(goapNode.poiTarget);
     }
     #endregion
+
+    private bool IsTargetMissing(ActualGoapNode node) {
+        //Different target missing for dig since, we need to allow the actor to dig block walls from another structure
+        //The only difference between this and the default target missing is when checking for neighbour, we will allow neighbours in a different structure
+
+        Character actor = node.actor;
+        IPointOfInterest poiTarget = node.poiTarget;
+        //Action is invalid if the target is unavailable and the action cannot be advertised if target is unavailable
+        if ((poiTarget.IsAvailable() == false && !canBeAdvertisedEvenIfTargetIsUnavailable) || poiTarget.gridTileLocation == null) {
+            return true;
+        }
+        if (actionLocationType != ACTION_LOCATION_TYPE.IN_PLACE && actor.currentRegion != poiTarget.gridTileLocation.structure.region) {
+            return true;
+        }
+        if (actionLocationType == ACTION_LOCATION_TYPE.NEAR_TARGET) {
+            //if the action type is NEAR_TARGET, then check if the actor is near the target, if not, this action is invalid.
+            if (actor.gridTileLocation != poiTarget.gridTileLocation && actor.gridTileLocation.IsNeighbour(poiTarget.gridTileLocation) == false) {
+                return true;
+            }
+        } else if (actionLocationType == ACTION_LOCATION_TYPE.NEAR_OTHER_TARGET) {
+            //if the action type is NEAR_TARGET, then check if the actor is near the target, if not, this action is invalid.
+            if (actor.gridTileLocation != node.targetTile && actor.gridTileLocation.IsNeighbour(node.targetTile) == false) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
