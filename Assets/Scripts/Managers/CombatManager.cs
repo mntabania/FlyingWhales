eaa24a7f4f6003b7878d3940089cdd53764cbc2e
Ian Chy <chrisian023@gmail.com>
2020-11-06 22:21:51 +0800

    <Combat Reasons> [Update Feature] [Update UI]
    - Updated UI in faction UI crimes tab
    - Initial combat reason integration: changed json file and added different reasons but not yet implemented.

--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -17,7 +17,18 @@ public class CombatManager : BaseMonoBehaviour {
     public const int pursueDuration = 4;
     public const string Hostility = "Hostility", Retaliation = "Retaliation", Berserked = "Berserked", Action = "Action",
         Threatened = "Threatened", Anger = "Anger", Join_Combat = "Join Combat", Drunk = "Drunk", Rage = "Rage", Demon_Kill = "Demon Kill", Dig = "Dig",
-        Avoiding_Witnesses = "Avoiding Witnesses", Encountered_Hostile = "Encountered Hostile";
+        Avoiding_Witnesses = "Avoiding Witnesses", Encountered_Hostile = "Encountered Hostile", Clear_Demonic_Intrusion = "Clear_Demonic_Intrusion";
+
+    //Hostility reasons
+    public const string Raid = "Raid", Warring_Factions = "Warring_Factions",
+        Slaying_Monster = "Slaying_Monster", Slaying_Undead = "Slaying_Undead", Fighting_Vagrant = "Fighting_Vagrant", Feral_Monster = "Feral_Monster",
+        Hostile_Undead = "Hostile_Undead", Brawl = "Brawl";
+
+    //Retaliation reasons
+    public const string Resisting_Arrest = "Resisting_Arrest", Resisting_Abduction = "Resisting_Abduction", Defending_Territory = "Defending_Territory", Defending_Self = "Defending_Self";
+
+
+
 
     [SerializeField] private ProjectileDictionary _projectileDictionary;
     [SerializeField] private GameObject _dragonProjectile;
