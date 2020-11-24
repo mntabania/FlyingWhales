75cd9f1a169212b059a21a79db15e5e08b1fcc34
Ian Chy <chrisian023@gmail.com>
2020-11-24 10:19:01 +0800

    <Paralysis Symptom Fix> [Bug Fix]
    - Should not be able to apply paralysis if character is already paralyzed

--- Assets/Scripts/Player/Plague Related Scripts/Symptoms/Paralysis.cs
+++ Assets/Scripts/Player/Plague Related Scripts/Symptoms/Paralysis.cs
@@ -8,10 +8,12 @@ namespace Plague.Symptom {
         public override PLAGUE_SYMPTOM symptomType => PLAGUE_SYMPTOM.Paralysis;
 
         protected override void ActivateSymptom(Character p_character) {
-            p_character.traitContainer.AddTrait(p_character, "Paralyzed");
-            Log log = GameManager.CreateNewLog(GameManager.Instance.Today(), "Character", "Plague", "plague_paralysis", null, LOG_TAG.Life_Changes);
-            log.AddToFillers(p_character, p_character.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
-            log.AddLogToDatabase();
+            if (!p_character.traitContainer.HasTrait("Paralyzed")) {
+                p_character.traitContainer.AddTrait(p_character, "Paralyzed");
+                Log log = GameManager.CreateNewLog(GameManager.Instance.Today(), "Character", "Plague", "plague_paralysis", null, LOG_TAG.Life_Changes);
+                log.AddToFillers(p_character, p_character.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
+                log.AddLogToDatabase();
+            }
             Debug.Log("Activated Paralysis Symptom");
         }
         public override void HourStarted (Character p_character, int p_numOfHoursPassed) {
