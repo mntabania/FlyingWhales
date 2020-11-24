e2c13e782aa40fbb11cb7df1afb74c7b3aee459f
Mykel Abania <mntabania@gmail.com>
2020-11-24 12:53:01 +0800

    <Pathfinding Stack Overflow Fix> [Bug Fixes] [Cleanup]
    - Cleaned up stroll outside state
    - [Live 0.33.46] StackOverflowException (https://trello.com/c/RkHWE8Fk/2844-live-03346-stackoverflowexception)
    - Corn crop always horizontal fix

--- Assets/Scripts/Locations/Inner Maps/Map Objects/Map Object Visuals/CornCropObjectVisual.cs
+++ Assets/Scripts/Locations/Inner Maps/Map Objects/Map Object Visuals/CornCropObjectVisual.cs
@@ -4,9 +4,15 @@ using UnityEngine.Assertions;
 
 public class CornCropObjectVisual : TileObjectGameObject {
     
-    [SerializeField] private Sprite empty;
-    [SerializeField] private Sprite growing;
-    [SerializeField] private Sprite harvestable;
+    [SerializeField] private Sprite horizontalEmpty;
+    [SerializeField] private Sprite horizontalGrowing;
+    [SerializeField] private Sprite horizontalHarvestable;
+    
+    [SerializeField] private Sprite verticalEmpty;
+    [SerializeField] private Sprite verticalGrowing;
+    [SerializeField] private Sprite verticalHarvestable;
+
+    private bool _isHorizontal;
     
     public override void UpdateTileObjectVisual(TileObject tileObject) {
         Crops crops = tileObject as Crops;
@@ -14,11 +20,15 @@ public class CornCropObjectVisual : TileObjectGameObject {
         int totalRipeningTicks = crops.GetRipeningTicks();
         int ticksGrowing = totalRipeningTicks - crops.remainingRipeningTicks; 
         if (ticksGrowing < Mathf.FloorToInt(totalRipeningTicks * 0.4f)) {
-            SetVisual(empty);
+            SetVisual(_isHorizontal ? horizontalEmpty : verticalEmpty);
         } else if (ticksGrowing < totalRipeningTicks) {
-            SetVisual(growing);    
+            SetVisual(_isHorizontal ? horizontalGrowing : verticalGrowing);    
         } else {
-            SetVisual(harvestable);
+            SetVisual(_isHorizontal ? horizontalHarvestable : verticalHarvestable);
         }
+    }
+    public override void SetVisual(Sprite sprite) {
+        base.SetVisual(sprite);
+        _isHorizontal = sprite.name.Contains("#1");
     }
 }
