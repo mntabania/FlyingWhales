1fa999dea60936021119e636d7cb523adc920d37
Ian Chy <chrisian023@gmail.com>
2020-12-03 23:06:21 +0800

    <Testing Utilities> [Update Feature]
    - Reduced hover info on location job queue if owner is ratmen, only show the location job queue itself

--- Assets/Scripts/UtilityScripts/TestingUtilities.cs
+++ Assets/Scripts/UtilityScripts/TestingUtilities.cs
@@ -10,27 +10,30 @@ namespace UtilityScripts {
             summary += "\nLocations Info:";
             for (int i = 0; i < settlements.Count; i++) {
                 NPCSettlement npcSettlement = settlements[i];
-                if (npcSettlement.locationType != LOCATION_TYPE.VILLAGE && (npcSettlement.owner == null || npcSettlement.owner.factionType.type != FACTION_TYPE.Ratmen)) {
+                bool isRatmanFaction = npcSettlement.owner?.factionType.type == FACTION_TYPE.Ratmen;
+                if (npcSettlement.locationType != LOCATION_TYPE.VILLAGE && !isRatmanFaction) {
                     continue;
                 }
-                summary += $"\n<b>{npcSettlement.name}</b> Settlement Type: {npcSettlement.settlementType?.settlementType.ToString() ?? "None"}";
-                summary += $"\nHas Peasants: {npcSettlement.hasPeasants.ToString()}, Has Workers: {npcSettlement.hasWorkers.ToString()}";
-                summary += $"\nStorage: {npcSettlement.mainStorage?.name ?? "None"}. Prison: {npcSettlement.prison?.name ?? "None"}";
-                // if (npcSettlement.settlementType != null) {
-                //     summary += $"\n<b>Max Dwellings: {npcSettlement.settlementType.maxDwellings.ToString()}</b>, <b>Max Facilities: {npcSettlement.settlementType.maxFacilities.ToString()}</b>";
-                //     summary += $"\n<b>Facility Weights and Caps:</b>";
-                //     foreach (var kvp in npcSettlement.settlementType.facilityWeights.dictionary) {
-                //         summary += $"\n\t{kvp.Key.ToString()} - {kvp.Value.ToString()} - {npcSettlement.settlementType.facilityCaps[kvp.Key].ToString()}";
-                //     }
-                // }
-                summary += $"\nNeeded Items: ";
-                for (int j = 0; j < npcSettlement.neededObjects.Count; j++) {
-                    summary += $"|{npcSettlement.neededObjects[j].ToString()}|";
-                }
-                summary += $"\nActive Events: ";
-                for (int j = 0; j < npcSettlement.eventManager.activeEvents.Count; j++) {
-                    SettlementEvent settlementEvent = npcSettlement.eventManager.activeEvents[j];
-                    summary += $"|{settlementEvent.GetTestingInfo()}|";
+                if (!isRatmanFaction) {
+                    summary += $"\n<b>{npcSettlement.name}</b> Settlement Type: {npcSettlement.settlementType?.settlementType.ToString() ?? "None"}";
+                    summary += $"\nHas Peasants: {npcSettlement.hasPeasants.ToString()}, Has Workers: {npcSettlement.hasWorkers.ToString()}";
+                    summary += $"\nStorage: {npcSettlement.mainStorage?.name ?? "None"}. Prison: {npcSettlement.prison?.name ?? "None"}";
+                    // if (npcSettlement.settlementType != null) {
+                    //     summary += $"\n<b>Max Dwellings: {npcSettlement.settlementType.maxDwellings.ToString()}</b>, <b>Max Facilities: {npcSettlement.settlementType.maxFacilities.ToString()}</b>";
+                    //     summary += $"\n<b>Facility Weights and Caps:</b>";
+                    //     foreach (var kvp in npcSettlement.settlementType.facilityWeights.dictionary) {
+                    //         summary += $"\n\t{kvp.Key.ToString()} - {kvp.Value.ToString()} - {npcSettlement.settlementType.facilityCaps[kvp.Key].ToString()}";
+                    //     }
+                    // }
+                    summary += $"\nNeeded Items: ";
+                    for (int j = 0; j < npcSettlement.neededObjects.Count; j++) {
+                        summary += $"|{npcSettlement.neededObjects[j].ToString()}|";
+                    }
+                    summary += $"\nActive Events: ";
+                    for (int j = 0; j < npcSettlement.eventManager.activeEvents.Count; j++) {
+                        SettlementEvent settlementEvent = npcSettlement.eventManager.activeEvents[j];
+                        summary += $"|{settlementEvent.GetTestingInfo()}|";
+                    }
                 }
                 if (npcSettlement.owner == null) { continue; }
                 summary += $"\n{npcSettlement.name} Location Job Queue:";
@@ -48,33 +51,35 @@ namespace UtilityScripts {
                 } else {
                     summary += "\nNone";
                 }
-                if (npcSettlement.owner != null) {
-                    summary += $"\n-----------------------------";
-                    summary += $"\n{npcSettlement.owner.name} Faction Job Queue:";
-                    if (npcSettlement.owner.availableJobs.Count > 0) {
-                        for (int j = 0; j < npcSettlement.owner.availableJobs.Count; j++) {
-                            JobQueueItem jqi = npcSettlement.owner.availableJobs[j];
-                            if (jqi is GoapPlanJob) {
-                                GoapPlanJob gpj = jqi as GoapPlanJob;
-                                summary += $"\n<b>{gpj.name} Targeting {gpj.targetPOI?.ToString() ?? "None"}</b>";
-                            } else {
-                                summary += $"\n<b>{jqi.name}</b>";
+                if (!isRatmanFaction) {
+                    if (npcSettlement.owner != null) {
+                        summary += $"\n-----------------------------";
+                        summary += $"\n{npcSettlement.owner.name} Faction Job Queue:";
+                        if (npcSettlement.owner.availableJobs.Count > 0) {
+                            for (int j = 0; j < npcSettlement.owner.availableJobs.Count; j++) {
+                                JobQueueItem jqi = npcSettlement.owner.availableJobs[j];
+                                if (jqi is GoapPlanJob) {
+                                    GoapPlanJob gpj = jqi as GoapPlanJob;
+                                    summary += $"\n<b>{gpj.name} Targeting {gpj.targetPOI?.ToString() ?? "None"}</b>";
+                                } else {
+                                    summary += $"\n<b>{jqi.name}</b>";
+                                }
+                                summary += $"\n Assigned Character: {jqi.assignedCharacter?.name}";
                             }
-                            summary += $"\n Assigned Character: {jqi.assignedCharacter?.name}";
+                        } else {
+                            summary += "\nNone";
                         }
-                    } else {
-                        summary += "\nNone";
-                    }
-                    summary += $"\n-----------------------------";
-                    summary += $"\n{npcSettlement.owner.name} Party Quests:";
-                    if (npcSettlement.owner.partyQuestBoard.availablePartyQuests.Count > 0) {
-                        for (int j = 0; j < npcSettlement.owner.partyQuestBoard.availablePartyQuests.Count; j++) {
-                            PartyQuest quest = npcSettlement.owner.partyQuestBoard.availablePartyQuests[j];
-                            summary += $"\n<b>{quest.partyQuestType.ToString()}</b>";
-                            summary += $"(Assigned Party: {quest.assignedParty?.partyName})";
+                        summary += $"\n-----------------------------";
+                        summary += $"\n{npcSettlement.owner.name} Party Quests:";
+                        if (npcSettlement.owner.partyQuestBoard.availablePartyQuests.Count > 0) {
+                            for (int j = 0; j < npcSettlement.owner.partyQuestBoard.availablePartyQuests.Count; j++) {
+                                PartyQuest quest = npcSettlement.owner.partyQuestBoard.availablePartyQuests[j];
+                                summary += $"\n<b>{quest.partyQuestType.ToString()}</b>";
+                                summary += $"(Assigned Party: {quest.assignedParty?.partyName})";
+                            }
+                        } else {
+                            summary += "\nNone";
                         }
-                    } else {
-                        summary += "\nNone";
                     }
                 }
                 summary += "\n";
