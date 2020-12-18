78e16097c4e412d37b97d2be1b857be0315e235f
Mykel Abania <mntabania@gmail.com>
2020-12-18 16:24:18 +0800

    <Dev Build 0.33.67> [Dev Build]
    - Increased project version 0.33.67

--- Assets/Scripts/Saving/SaveUtilities.cs
+++ Assets/Scripts/Saving/SaveUtilities.cs
@@ -13,7 +13,7 @@ public static class SaveUtilities {
 
 
     public static List<string> compatibleSaveFileVersions = new List<string>() {
-        "0.33.66"
+        "0.33.67"
     };
     
     #region Character States
