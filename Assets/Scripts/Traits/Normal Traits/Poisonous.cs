e597ecfdd52a6ed76ea10efbeaa24b5faad691fd
Ian Chy <chrisian023@gmail.com>
2020-12-22 16:59:28 +0800

    <Poisonous Bug Fix> [Update Feature]
    - Fixed: Poisonous trait does not add trait per hour
    - Poisonous trait should add max Poisoned trait

--- Assets/Scripts/Traits/Normal Traits/Poisonous.cs
+++ Assets/Scripts/Traits/Normal Traits/Poisonous.cs
@@ -12,9 +12,16 @@ namespace Traits {
             type = TRAIT_TYPE.BUFF;
             effect = TRAIT_EFFECT.NEUTRAL;
             ticksDuration = 0;
+            AddTraitOverrideFunctionIdentifier(TraitManager.Hour_Started_Trait);
         }
 
         #region Overrides
+        public override void OnAddTrait(ITraitable addedTo) {
+            base.OnAddTrait(addedTo);
+            for (int i = 0; i < 3; i++) {
+                addedTo.traitContainer.AddTrait(addedTo, "Poisoned", bypassElementalChance: true, overrideDuration: 0);
+            }
+        }
         public override void OnHourStarted(ITraitable traitable) {
             base.OnHourStarted(traitable);
             traitable.traitContainer.AddTrait(traitable, "Poisoned", bypassElementalChance: true, overrideDuration: 0);
