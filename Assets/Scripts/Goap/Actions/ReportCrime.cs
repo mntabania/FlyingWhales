de1de07a6ca12d2e2406d76209856b0442b6c126
Ian Chy <chrisian023@gmail.com>
2021-01-04 23:59:29 +0800

    <Report Crime Changes> [Update Feature]
    - Report crime should be a major action

--- Assets/Scripts/Goap/Actions/ReportCrime.cs
+++ Assets/Scripts/Goap/Actions/ReportCrime.cs
@@ -13,7 +13,7 @@ public class ReportCrime : GoapAction {
         actionIconString = GoapActionStateDB.Report_Icon;
         advertisedBy = new POINT_OF_INTEREST_TYPE[] { POINT_OF_INTEREST_TYPE.CHARACTER };
         racesThatCanDoAction = new RACE[] { RACE.HUMANS, RACE.ELVES, RACE.GOBLIN, RACE.FAERY, RACE.RATMAN };
-        logTags = new[] {LOG_TAG.Crimes};
+        logTags = new[] {LOG_TAG.Crimes, LOG_TAG.Major};
     }
 
     #region Overrides
