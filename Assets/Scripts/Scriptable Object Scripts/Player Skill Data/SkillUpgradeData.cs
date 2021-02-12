b4194484bf30e1ce6bd12ac0ad1ba1f66b43da21
eonaranas <aaronaranas@gmail.com>
2021-02-12 14:51:08 +0800

    <skill editior update> [update]
    - enable input only one data for fields

--- Assets/Scripts/Scriptable Object Scripts/Player Skill Data/SkillUpgradeData.cs
+++ Assets/Scripts/Scriptable Object Scripts/Player Skill Data/SkillUpgradeData.cs
@@ -48,11 +48,17 @@ public class SkillUpgradeData
         if (upgradeCosts == null || upgradeCosts.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= upgradeCosts.Count) {
+            return upgradeCosts[upgradeCosts.Count - 1];
+        }
         return upgradeCosts[p_currentLevel];
     }
     public int GetChargesBaseOnLevel(int p_currentLevel) {
         if (chargesPerLevel == null || chargesPerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= chargesPerLevel.Count) {
+            return chargesPerLevel[upgradeCosts.Count - 1];
         }
         return chargesPerLevel[p_currentLevel];
     }
@@ -60,11 +66,17 @@ public class SkillUpgradeData
         if (additionalDamagePerLevel == null || additionalDamagePerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= additionalDamagePerLevel.Count) {
+            return additionalDamagePerLevel[upgradeCosts.Count - 1];
+        }
         return additionalDamagePerLevel[p_currentLevel];
     }
     public float GetAdditionalPiercePerLevelBaseOnLevel(int p_currentLevel) {
         if (additionalPiercePerLevel == null || additionalPiercePerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= additionalPiercePerLevel.Count) {
+            return additionalPiercePerLevel[upgradeCosts.Count - 1];
         }
         return additionalPiercePerLevel[p_currentLevel];
     }
@@ -72,11 +84,17 @@ public class SkillUpgradeData
         if (additionalHpPercentagePerLevel == null || additionalHpPercentagePerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= additionalHpPercentagePerLevel.Count) {
+            return additionalHpPercentagePerLevel[upgradeCosts.Count - 1];
+        }
         return additionalHpPercentagePerLevel[p_currentLevel];
     }
     public int GetAdditionalMaxHpActualPerLevelBaseOnLevel(int p_currentLevel) {
         if (additionalMaxHPActualPerLevel == null || additionalMaxHPActualPerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= additionalMaxHPActualPerLevel.Count) {
+            return additionalMaxHPActualPerLevel[upgradeCosts.Count - 1];
         }
         return additionalMaxHPActualPerLevel[p_currentLevel];
     }
@@ -84,11 +102,17 @@ public class SkillUpgradeData
         if (additionalMaxHPPercentagePerLevel == null || additionalMaxHPPercentagePerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= additionalMaxHPPercentagePerLevel.Count) {
+            return additionalMaxHPPercentagePerLevel[upgradeCosts.Count - 1];
+        }
         return additionalMaxHPPercentagePerLevel[p_currentLevel];
     }
     public int GetAdditionalHpActualPerLevelBaseOnLevel(int p_currentLevel) {
         if (additionalHpValuePerLevel == null || additionalHpValuePerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= additionalHpValuePerLevel.Count) {
+            return additionalHpValuePerLevel[upgradeCosts.Count - 1];
         }
         return additionalHpValuePerLevel[p_currentLevel];
     }
@@ -96,11 +120,17 @@ public class SkillUpgradeData
         if (additionalAttackPercentagePerLevel == null || additionalAttackPercentagePerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= additionalAttackPercentagePerLevel.Count) {
+            return additionalAttackPercentagePerLevel[upgradeCosts.Count - 1];
+        }
         return additionalAttackPercentagePerLevel[p_currentLevel];
     }
     public int GetAdditionalAttackActualPerLevelBaseOnLevel(int p_currentLevel) {
         if (additionalAttackValuePerLevel == null || additionalAttackValuePerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= additionalAttackValuePerLevel.Count) {
+            return additionalAttackValuePerLevel[upgradeCosts.Count - 1];
         }
         return additionalAttackValuePerLevel[p_currentLevel];
     }
@@ -108,11 +138,17 @@ public class SkillUpgradeData
         if (manacostPerLevel == null || manacostPerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= manacostPerLevel.Count) {
+            return manacostPerLevel[upgradeCosts.Count - 1];
+        }
         return manacostPerLevel[p_currentLevel];
     }
     public float GetIncreaseStatsPercentagePerLevel(int p_currentLevel) {
         if (statsIncreasedPercentagePerLevel == null || statsIncreasedPercentagePerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= statsIncreasedPercentagePerLevel.Count) {
+            return statsIncreasedPercentagePerLevel[upgradeCosts.Count - 1];
         }
         return statsIncreasedPercentagePerLevel[p_currentLevel];
     }
@@ -120,11 +156,17 @@ public class SkillUpgradeData
         if (durationBonusPerLevel == null || durationBonusPerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= durationBonusPerLevel.Count) {
+            return durationBonusPerLevel[upgradeCosts.Count - 1];
+        }
         return durationBonusPerLevel[p_currentLevel];
     }
     public int GetTileRangeBonusPerLevel(int p_currentLevel) {
         if (additionalTileRangeBonusPerLevel == null || additionalTileRangeBonusPerLevel.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= additionalTileRangeBonusPerLevel.Count) {
+            return additionalTileRangeBonusPerLevel[upgradeCosts.Count - 1];
         }
         return additionalTileRangeBonusPerLevel[p_currentLevel];
     }
@@ -132,17 +174,26 @@ public class SkillUpgradeData
         if (additionalChanceBonusPercentagePerLevel == null || additionalChanceBonusPercentagePerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= additionalChanceBonusPercentagePerLevel.Count) {
+            return additionalChanceBonusPercentagePerLevel[upgradeCosts.Count - 1];
+        }
         return additionalChanceBonusPercentagePerLevel[p_currentLevel];
     }
     public int GetCoolDownPerLevel(int p_currentLevel) {
         if (cooldownPerLevel == null || cooldownPerLevel.Count <= 0) {
             return 0;
         }
+        if (p_currentLevel >= cooldownPerLevel.Count) {
+            return cooldownPerLevel[upgradeCosts.Count - 1];
+        }
         return cooldownPerLevel[p_currentLevel];
     }
     public int GetSkillMovementSpeedDownPerLevel(int p_currentLevel) {
         if (skillMovementSpeed == null || skillMovementSpeed.Count <= 0) {
             return 0;
+        }
+        if (p_currentLevel >= skillMovementSpeed.Count) {
+            return skillMovementSpeed[upgradeCosts.Count - 1];
         }
         return skillMovementSpeed[p_currentLevel];
     }
