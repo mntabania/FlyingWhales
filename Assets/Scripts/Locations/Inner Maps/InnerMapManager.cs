2f1f1b294d21c799a17e8e63438766ff3b724405
Mykel Abania <mntabania@gmail.com>
2021-03-16 13:13:23 +0800

    <Spire UI Blocker Fix> [Bug Fixes]
    - Spire UI blocker when accessing Spells tab fix

--- Assets/Scripts/Locations/Inner Maps/InnerMapManager.cs
+++ Assets/Scripts/Locations/Inner Maps/InnerMapManager.cs
@@ -385,6 +385,8 @@ namespace Inner_Maps {
             // summary = $"{summary}\n<b>Path Area:</b>{tile.graphNode?.Area.ToString()}";
             // summary = $"{summary}\n<b>Is Path Possible to Selected Character:</b>{isPathPossible.ToString()}";
             summary = $"{summary}\n<b>Area:</b>{(area.name ?? "None")}";
+            summary = $"{summary}\n<b>Settlement on Area:</b>{(area.settlementOnArea?.name ?? "None")}";
+            
             // summary = $"{summary}\n<b>Local Location:</b>{tile.localLocation.ToString()}";
             // summary = $"{summary} <b>World Location:</b>{tile.worldLocation.ToString()}";
             // summary = $"{summary} <b>Centered World Location:</b>{tile.centeredWorldLocation.ToString()}";
