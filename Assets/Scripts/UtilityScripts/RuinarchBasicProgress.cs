ef5230b3898c3282a94446793f65e762831aeb03
Mykel Abania <mntabania@gmail.com>
2021-03-18 22:34:59 +0800

    <Ruinarch Basic Progress> [New Feature]
    - Ruinarch Basic Progress For Ruinarch's Awakening

---
+++ Assets/Scripts/UtilityScripts/RuinarchBasicProgress.cs
@@ -0,0 +1,10 @@
+﻿namespace UtilityScripts {
+    public class RuinarchBasicProgress : RuinarchProgressable {
+        public override string name { get; }
+        public override BOOKMARK_TYPE bookmarkType => BOOKMARK_TYPE.Progress_Bar;
+
+        public RuinarchBasicProgress(string p_name) {
+            name = p_name;
+        }
+    }
+}
