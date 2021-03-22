2a5eb0756a51d828b9ff769732f47d6bd4b35e91
eonaranas <aaronaranas@gmail.com>
2021-03-22 17:10:56 +0800

    <Visual update> [asset update]
    - earth spike asset update
    - water spike asset update
    
    <Check character if existing>
    [double check]
    
    - null checking

--- Assets/Scripts/Player/Skills/Player Actions/AfflictData.cs
+++ Assets/Scripts/Player/Skills/Player Actions/AfflictData.cs
@@ -52,8 +52,10 @@ public class AfflictData : PlayerAction {
     protected void AfflictPOIWith(string traitName, IPointOfInterest target, string logName, int overridenDuration = 0) {
         if (PlayerSkillManager.Instance.afflictionsNameSkillTypeDictionary.ContainsKey(traitName)) {
             PLAYER_SKILL_TYPE skillType = PlayerSkillManager.Instance.afflictionsNameSkillTypeDictionary[traitName];
-            if (!(target as Character).afflictionsSkillsInflictedByPlayer.Contains(skillType)) {
-                (target as Character).afflictionsSkillsInflictedByPlayer.Add(skillType);
+            if (target is Character character) {
+                if (!character.afflictionsSkillsInflictedByPlayer.Contains(skillType)) {
+                    character.afflictionsSkillsInflictedByPlayer.Add(skillType);
+                }
             }
         }
         if (overridenDuration > 0) {
