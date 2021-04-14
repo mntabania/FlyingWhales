06919e9af26a276c920c8f196bf956a15fbc0baf
Mykel Abania <mntabania@gmail.com>
2021-04-14 20:43:58 +0800

    <New Spell - Empower> [New Feature]
    - New Spell - Empower (https://trello.com/c/RlSNDkXj/4061-new-spell-empower)

--- Assets/Scripts/Player/Skills/Player Actions/EmpowerData.cs
+++ Assets/Scripts/Player/Skills/Player Actions/EmpowerData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UtilityScripts;
 
 public class EmpowerData : PlayerAction {
     public override PLAYER_SKILL_TYPE type => PLAYER_SKILL_TYPE.EMPOWER;
@@ -8,5 +9,29 @@ public class EmpowerData : PlayerAction {
     public override string description => "This Action will significantly increase a character's combat prowess for a short amount of time.";
     public EmpowerData() : base() {
         targetTypes = new SPELL_TARGET[] { SPELL_TARGET.CHARACTER };
+    }
+    public override void ActivateAbility(IPointOfInterest targetPOI) {
+        if (targetPOI is Character targetCharacter) {
+            if (targetCharacter.traitContainer.AddTrait(targetCharacter, "Empowered")) {
+                Log log = GameManager.CreateNewLog(GameManager.Instance.Today(), "InterventionAbility", "Empower", "activated", null, LOG_TAG.Player);
+                log.AddToFillers(targetCharacter, targetCharacter.name, LOG_IDENTIFIER.ACTIVE_CHARACTER);
+                log.AddLogToDatabase();
+                PlayerManager.Instance.player.ShowNotificationFromPlayer(log);
+            }
+            base.ActivateAbility(targetPOI);
+        }
+    }
+    public override bool CanPerformAbilityTowards(Character targetCharacter) {
+        if (targetCharacter.traitContainer.HasTrait("Empowered")) {
+            return false;
+        }
+        return base.CanPerformAbilityTowards(targetCharacter);
+    }
+    public override string GetReasonsWhyCannotPerformAbilityTowards(Character targetCharacter) {
+        string reasons = base.GetReasonsWhyCannotPerformAbilityTowards(targetCharacter);
+        if (targetCharacter.traitContainer.HasTrait("Agoraphobic")) {
+            reasons += $"{targetCharacter.name} already has this Flaw,";
+        }
+        return reasons;
     }
 }
