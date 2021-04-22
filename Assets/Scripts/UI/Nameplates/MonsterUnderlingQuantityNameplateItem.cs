2f74b7e2562f944eb171af96d518f55ccecc4b2f
chrisian23 <chrisian023@gmail.com>
2021-04-22 08:14:10 +0800

    <Monster and Demon Underling Visual Cooldown> [Update Feature]
    - Visual cooldowns for monster and demon underlings

--- Assets/Scripts/UI/Nameplates/MonsterUnderlingQuantityNameplateItem.cs
+++ Assets/Scripts/UI/Nameplates/MonsterUnderlingQuantityNameplateItem.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class MonsterUnderlingQuantityNameplateItem : NameplateItem<MonsterAndDemonUnderlingCharges> {
 
@@ -13,17 +14,24 @@ public class MonsterUnderlingQuantityNameplateItem : NameplateItem<MonsterAndDem
     [SerializeField] private RuinarchText txtAttack;
     [SerializeField] private RuinarchText txtAttackSpeed;
     [SerializeField] private RuinarchText txtManaCost;
+    [SerializeField] private Image cooldownCoverImage;
 
     private MonsterAndDemonUnderlingCharges _monsterOrMinion;
+    private MinionPlayerSkill _demonPlayerSkill;
     public override MonsterAndDemonUnderlingCharges obj => _monsterOrMinion;
     public int summonCost { get; private set; }
 
     public override void SetObject(MonsterAndDemonUnderlingCharges o) {
         base.SetObject(o);
         _monsterOrMinion = o;
+        if (_monsterOrMinion.isDemon) {
+            _demonPlayerSkill = PlayerSkillManager.Instance.GetMinionPlayerSkillDataByMinionType(_monsterOrMinion.minionType);
+        }
         UpdateVisuals();
         UpdateMainText();
         UpdateBasicData();
+        UpdateCooldownState();
+        SubscribeToSignals();
     }
     public void UpdateBasicData() {
         UpdateMainText();
@@ -55,9 +63,91 @@ public class MonsterUnderlingQuantityNameplateItem : NameplateItem<MonsterAndDem
         m_displayMaxChrageText = _monsterOrMinion.maxCharges;
         subLbl.text = currentCharges + "/" + _monsterOrMinion.maxCharges;
     }
+    private void SubscribeToSignals() {
+        Messenger.AddListener<SkillData>(PlayerSkillSignals.SPELL_COOLDOWN_STARTED, OnSpellCooldownStarted);
+        Messenger.AddListener<SkillData>(PlayerSkillSignals.SPELL_COOLDOWN_FINISHED, OnSpellCooldownFinished);
+        Messenger.AddListener<MonsterAndDemonUnderlingCharges>(PlayerSkillSignals.START_MONSTER_UNDERLING_COOLDOWN, OnStartMonsterUnderlingCooldown);
+        Messenger.AddListener<MonsterAndDemonUnderlingCharges>(PlayerSkillSignals.STOP_MONSTER_UNDERLING_COOLDOWN, OnStopMonsterUnderlingCooldown);
+        Messenger.AddListener<MinionPlayerSkill>(PlayerSkillSignals.PER_TICK_DEMON_COOLDOWN, PerTickDemonCooldownListener);
+        Messenger.AddListener<MonsterAndDemonUnderlingCharges>(PlayerSkillSignals.PER_TICK_MONSTER_UNDERLING_COOLDOWN, PerTickMonsterUnderlingCooldownListener);
+
+    }
+    private void UnsubscribeToSignals() {
+        Messenger.RemoveListener<SkillData>(PlayerSkillSignals.SPELL_COOLDOWN_STARTED, OnSpellCooldownStarted);
+        Messenger.RemoveListener<SkillData>(PlayerSkillSignals.SPELL_COOLDOWN_FINISHED, OnSpellCooldownFinished);
+        Messenger.RemoveListener<MonsterAndDemonUnderlingCharges>(PlayerSkillSignals.START_MONSTER_UNDERLING_COOLDOWN, OnStartMonsterUnderlingCooldown);
+        Messenger.RemoveListener<MonsterAndDemonUnderlingCharges>(PlayerSkillSignals.STOP_MONSTER_UNDERLING_COOLDOWN, OnStopMonsterUnderlingCooldown);
+        Messenger.RemoveListener<MinionPlayerSkill>(PlayerSkillSignals.PER_TICK_DEMON_COOLDOWN, PerTickDemonCooldownListener);
+        Messenger.RemoveListener<MonsterAndDemonUnderlingCharges>(PlayerSkillSignals.PER_TICK_MONSTER_UNDERLING_COOLDOWN, PerTickMonsterUnderlingCooldownListener);
+    }
+
+    #region Listeners
+    private void OnSpellCooldownStarted(SkillData data) {
+        if (_demonPlayerSkill == data) {
+            StartCooldownFill();
+        }
+    }
+    private void OnSpellCooldownFinished(SkillData data) {
+        if (_demonPlayerSkill == data) {
+            StopCooldownFill();
+        }
+    }
+    private void OnStartMonsterUnderlingCooldown(MonsterAndDemonUnderlingCharges data) {
+        if (_monsterOrMinion == data) {
+            StartCooldownFill();
+        }
+    }
+    private void OnStopMonsterUnderlingCooldown(MonsterAndDemonUnderlingCharges data) {
+        if (_monsterOrMinion == data) {
+            StopCooldownFill();
+        }
+    }
+    private void PerTickMonsterUnderlingCooldownListener(MonsterAndDemonUnderlingCharges data) {
+        if (_monsterOrMinion == data) {
+            PerTickCooldownMonster();
+        }
+    }
+    private void PerTickDemonCooldownListener(MinionPlayerSkill data) {
+        if (_demonPlayerSkill == data) {
+            PerTickCooldownDemon();
+        }
+    }
+    #endregion
+
+    #region Cooldown
+    private void UpdateCooldownState() {
+        if (_monsterOrMinion.isReplenishing) {
+            StartCooldownFill();
+            PerTickCooldownMonster();
+        } else if (_demonPlayerSkill != null && _demonPlayerSkill.isInCooldown) {
+            StartCooldownFill();
+            PerTickCooldownDemon();
+        } else {
+            StopCooldownFill();
+        }
+    }
+    private void StartCooldownFill() {
+        cooldownCoverImage.fillAmount = 0f;
+        cooldownCoverImage.gameObject.SetActive(true);
+    }
+    private void PerTickCooldownDemon() {
+        float fillAmount = ((float) _demonPlayerSkill.currentCooldownTick / _demonPlayerSkill.cooldown);
+        cooldownCoverImage.DOFillAmount(fillAmount, 0.4f);
+    }
+    private void PerTickCooldownMonster() {
+        float fillAmount = ((float) _monsterOrMinion.currentCooldownTick / _monsterOrMinion.cooldown);
+        cooldownCoverImage.DOFillAmount(fillAmount, 0.4f);
+    }
+    private void StopCooldownFill() {
+        cooldownCoverImage.fillAmount = 0f;
+        cooldownCoverImage.gameObject.SetActive(false);
+    }
+    #endregion
 
     public override void Reset() {
         base.Reset();
         _monsterOrMinion = null;
+        StopCooldownFill();
+        UnsubscribeToSignals();
     }
 }
