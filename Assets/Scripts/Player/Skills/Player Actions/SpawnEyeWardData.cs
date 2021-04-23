de100403ecf9df71d9b63e129738d1972e1b0c59
chrisian23 <chrisian023@gmail.com>
2021-04-23 17:56:33 +0800

    <Spawn Eye Fixes> [Bug Fixes]
    - Spawn eye ward should be placeable on a tile even if there is still object if that object is not built

--- Assets/Scripts/Player/Skills/Player Actions/SpawnEyeWardData.cs
+++ Assets/Scripts/Player/Skills/Player Actions/SpawnEyeWardData.cs
@@ -42,7 +42,7 @@ public class SpawnEyeWardData : PlayerAction {
     public override bool CanPerformAbilityTowards(LocationGridTile targetTile, out string o_cannotPerformReason) {
         bool canPerform = base.CanPerformAbilityTowards(targetTile, out o_cannotPerformReason);
         if (canPerform) {
-            if(targetTile.tileObjectComponent.hiddenObjHere != null || targetTile.tileObjectComponent.objHere != null || !targetTile.IsPassable()) {
+            if(targetTile.tileObjectComponent.hiddenObjHere != null || (targetTile.tileObjectComponent.objHere != null && targetTile.tileObjectComponent.objHere.mapObjectState == MAP_OBJECT_STATE.BUILT) || !targetTile.IsPassable()) {
                 o_cannotPerformReason = LocalizationManager.Instance.GetLocalizedValue("Skills", "Spawn Eye Ward", "invalid_already_has_hidden_object");
                 return false;
             }
