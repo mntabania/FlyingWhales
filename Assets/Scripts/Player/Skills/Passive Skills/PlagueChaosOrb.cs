595a408b3f8dcd52d65ba775874882b5bb707dce
Mykel Abania <mntabania@gmail.com>
2021-04-23 14:55:31 +0800

    <Merge Fixes> [Bug Fixes]
    - Minor Merge Fixes

--- Assets/Scripts/Player/Skills/Passive Skills/PlagueChaosOrb.cs
+++ Assets/Scripts/Player/Skills/Passive Skills/PlagueChaosOrb.cs
@@ -13,7 +13,7 @@ public class PlagueChaosOrb : PassiveSkill {
     }
     private void OnTraitAdded(Character character, Trait trait) {
         if (GameUtilities.RollChance(50)) {
-        if (trait.name == "Depressed" || trait.name == "Insomnia" || trait.name == "Lethargic") { //|| trait.name == "Paralyzed"
+            if (trait.name == "Depressed" || trait.name == "Insomnia" || trait.name == "Lethargic") { //|| trait.name == "Paralyzed"
                 Messenger.Broadcast(PlayerSignals.CREATE_CHAOS_ORBS, character.worldPosition, 1, character.gridTileLocation.parentMap);
             }
         }
