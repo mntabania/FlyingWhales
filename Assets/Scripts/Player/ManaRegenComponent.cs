37a2119569fb1c9426d80c1c30644911173cb54a
Mykel Abania <mntabania@gmail.com>
2021-04-30 17:02:58 +0800

    <Various Bug Fixes> [Bug Fixes]
    - Prison increased room size
    - Max Mana not being saved fix
    - Let Go log fix
    - Damage particles not destroyed on death fix

--- Assets/Scripts/Player/ManaRegenComponent.cs
+++ Assets/Scripts/Player/ManaRegenComponent.cs
@@ -8,14 +8,18 @@ public class ManaRegenComponent
     private Player m_player;
 
     private int m_manaPitCount = 0;
+    private int m_maxMana = 0; //TODO: Move this to player pag may time na
 
     public ManaRegenComponent(Player p_player) {
         m_player = p_player;
+        m_maxMana = EditableValuesManager.Instance.maximumMana;
         SubscribeListeners();
     }
     public ManaRegenComponent(Player p_player, SaveDataManaRegenComponent data) {
         m_player = p_player;
         m_manaPitCount = data.manaPitCount;
+        m_maxMana = data.maxMana;
+        EditableValuesManager.Instance.maximumMana = m_maxMana;
         SubscribeListeners();
     }
     private void SubscribeListeners() {
@@ -29,6 +33,7 @@ public class ManaRegenComponent
     void OnStructurePlaced(LocationStructure p_structure) {
         if (p_structure.structureType == STRUCTURE_TYPE.MANA_PIT) {
             EditableValuesManager.Instance.maximumMana += EditableValuesManager.Instance.GetAdditionalMaxManaPerManaPit();
+            m_maxMana = EditableValuesManager.Instance.maximumMana;
             m_manaPitCount++;
         }
     }
@@ -36,6 +41,7 @@ public class ManaRegenComponent
     void OnStructureDestroyed(LocationStructure p_structure, Area p_area) {
         if (p_structure.structureType == STRUCTURE_TYPE.MANA_PIT) {
             EditableValuesManager.Instance.maximumMana -= EditableValuesManager.Instance.GetAdditionalMaxManaPerManaPit();
+            m_maxMana = EditableValuesManager.Instance.maximumMana;
             if (m_player.mana > EditableValuesManager.Instance.maximumMana) {
                 m_player.AdjustMana(EditableValuesManager.Instance.maximumMana - m_player.mana);
             }
@@ -45,6 +51,9 @@ public class ManaRegenComponent
 
     public int GetManaPitCount() {
         return m_manaPitCount;
+    }
+    public int GetMaxMana() {
+        return m_maxMana;
     }
 
     void OnHourStared() {
@@ -58,9 +67,11 @@ public class ManaRegenComponent
 #region Save Data
 public class SaveDataManaRegenComponent : SaveData<ManaRegenComponent> {
     public int manaPitCount;
+    public int maxMana;
     public override void Save(ManaRegenComponent data) {
         base.Save(data);
         manaPitCount = data.GetManaPitCount();
+        maxMana = data.GetMaxMana();
     }
 }
 #endregion
