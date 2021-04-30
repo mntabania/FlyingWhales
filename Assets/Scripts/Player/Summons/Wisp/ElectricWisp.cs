4a0a07905ca72d227c9504fca78397c94102a138
chrisian23 <chrisian023@gmail.com>
2021-04-30 18:04:52 +0800

    <Wisp Updates> [Update Feature]
    - Wisp should be Defend combat mode by default

--- Assets/Scripts/Player/Summons/Wisp/ElectricWisp.cs
+++ Assets/Scripts/Player/Summons/Wisp/ElectricWisp.cs
@@ -1,4 +1,5 @@
 ﻿public class ElectricWisp : Wisp {
+    public override COMBAT_MODE defaultCombatMode => COMBAT_MODE.Defend;
     public ElectricWisp() : base(SUMMON_TYPE.Electric_Wisp, "Electric Wisp") { }
     public ElectricWisp(string className) : base(SUMMON_TYPE.Electric_Wisp, className) { }
     public ElectricWisp(SaveDataSummon data) : base(data) { }
