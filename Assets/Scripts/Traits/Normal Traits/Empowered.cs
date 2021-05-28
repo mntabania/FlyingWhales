9f0189e720e0573a0ec1046ba933efa352c58204
chrisian23 <chrisian023@gmail.com>
2021-05-28 15:15:33 +0800

    <Empowered Status Change> [Update Feature]
    - Empowered status effect changes because attack and hp formula has changed

--- Assets/Scripts/Traits/Normal Traits/Empowered.cs
+++ Assets/Scripts/Traits/Normal Traits/Empowered.cs
@@ -4,7 +4,7 @@ using UnityEngine.Assertions;
 namespace Traits {
     public class Empowered : Status {
 
-        public int modification { get; private set; }
+        public float modificationPercent { get; private set; }
         
         public override Type serializedData => typeof(SaveDataEmpowered);
         public Empowered() {
@@ -19,21 +19,21 @@ namespace Traits {
         public override void LoadSecondWaveInstancedTrait(SaveDataTrait p_saveDataTrait) {
             base.LoadSecondWaveInstancedTrait(p_saveDataTrait);
             SaveDataEmpowered saveDataEmpowered = p_saveDataTrait as SaveDataEmpowered;
-            modification = saveDataEmpowered.modification;
+            modificationPercent = saveDataEmpowered.modificationPercent;
         }
         public override void OnAddTrait(ITraitable addedTo) {
             base.OnAddTrait(addedTo);
             if (addedTo is Character targetCharacter) {
-                float modificationPercent = PlayerSkillManager.Instance.GetAdditionalAttackPercentagePerLevelBaseOnLevel(PLAYER_SKILL_TYPE.EMPOWER)/100f;
-                modification = (int)(modificationPercent * targetCharacter.combatComponent.attack);
-                targetCharacter.combatComponent.AdjustAttackModifier(modification);
+                //float modificationPercent = PlayerSkillManager.Instance.GetAdditionalAttackPercentagePerLevelBaseOnLevel(PLAYER_SKILL_TYPE.EMPOWER)/100f;
+                modificationPercent = PlayerSkillManager.Instance.GetAdditionalAttackPercentagePerLevelBaseOnLevel(PLAYER_SKILL_TYPE.EMPOWER); //(int)(modificationPercent * targetCharacter.combatComponent.attack);
+                targetCharacter.combatComponent.AdjustAttackPercentModifier(modificationPercent);
             }
         }
         
         public override void OnRemoveTrait(ITraitable removedFrom, Character removedBy) {
             base.OnRemoveTrait(removedFrom, removedBy);
             if (removedFrom is Character targetCharacter) {
-                targetCharacter.combatComponent.AdjustAttackModifier(-modification);
+                targetCharacter.combatComponent.AdjustAttackPercentModifier(-modificationPercent);
             }
         }
         #endregion
@@ -42,12 +42,12 @@ namespace Traits {
 
 #region Save Data
 public class SaveDataEmpowered : SaveDataTrait {
-    public int modification;
+    public float modificationPercent;
     public override void Save(Trait trait) {
         base.Save(trait);
         Empowered data = trait as Empowered;
         Assert.IsNotNull(data);
-        modification = data.modification;
+        modificationPercent = data.modificationPercent;
     }
 }
 #endregion
