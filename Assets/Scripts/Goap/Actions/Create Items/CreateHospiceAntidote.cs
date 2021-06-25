fb4b23321696701bbe3810664cd05797742df8db
chrisian23 <chrisian023@gmail.com>
2021-06-25 14:55:33 +0800

    <Hospice Craft Potion/Antidote Updates> [Update Feature]
    - Crafters should also gain coins after successfully crafting antidote/potion in hospice

--- Assets/Scripts/Goap/Actions/Create Items/CreateHospiceAntidote.cs
+++ Assets/Scripts/Goap/Actions/Create Items/CreateHospiceAntidote.cs
@@ -37,6 +37,7 @@ public class CreateHospiceAntidote : GoapAction {
 
     #region State Effects
     public void AfterCreateHospiceAntidoteSuccess(ActualGoapNode p_node) {
+        p_node.actor.moneyComponent.AdjustCoins(10);
         TileObject antidote = InnerMapManager.Instance.CreateNewTileObject<TileObject>(TILE_OBJECT_TYPE.ANTIDOTE);
         LocationGridTile tileToSpawnPile = p_node.actor.gridTileLocation;
         if (tileToSpawnPile != null && tileToSpawnPile.tileObjectComponent.objHere != null) {
