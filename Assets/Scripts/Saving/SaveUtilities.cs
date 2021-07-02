8b114a7edfbd077bbf98be7472dfc950002f0113
Mykel Abania <mntabania@gmail.com>
2021-07-02 23:15:25 +0800

    <Updated Version to v0.5>

--- Assets/Scripts/Saving/SaveUtilities.cs
+++ Assets/Scripts/Saving/SaveUtilities.cs
@@ -13,7 +13,7 @@ public static class SaveUtilities {
 
 
     public static List<string> compatibleSaveFileVersions = new List<string>() {
-        "0.4.02", "0.4.03"
+        "0.5"
     };
     
     #region Character States
